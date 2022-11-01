a57e2a69fcc52ec706e0d729b219d54c5511b1e9
Naykyr Oliveira da Silva <103430603+NaykyrOliveira@users.noreply.github.com>
2022-11-01 10:36:15 -0300

    Herança

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ import {Conta} from "./Conta.js";
 
 const cliente1 = new Cliente("Ricardo", 11122233309);
 
-const ContaCorrenteRicardo = new Conta(0, cliente1, 1001);
+const ContaCorrenteRicardo = new ContaCorrente(cliente1, 1001);
 ContaCorrenteRicardo.depositar(500);
 ContaCorrenteRicardo.sacar(100);
 
-const contaPoupanca = new Conta(50, cliente1, 1001);
+const contaPoupanca = new ContaPoupanca(50, cliente1, 1001);
 
 console.log(contaPoupanca);
 console.log(ContaCorrenteRicardo);
