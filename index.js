a32a4c7a233309571c1d7e23a8bc9330cf84b83e
Naykyr Oliveira da Silva <103430603+NaykyrOliveira@users.noreply.github.com>
2022-11-03 10:26:05 -0300

    Criando funcionários

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import {Cliente} from "./Cliente.js";
-import {ContaCorrente} from "./ContaCorrente.js";
-import { ContaEmpresarial } from "./ContaEmpresarial.js";
-import {ContaPoupanca} from "./ContaPoupanca.js";
-import { ContaSalario } from "./ContaSalario.js";
+import {ContaCorrente} from "./Conta/ContaCorrente.js";
+import { ContaEmpresarial } from "./Conta/ContaEmpresarial.js";
+import {ContaPoupanca} from "./Conta/ContaPoupanca.js";
+import { ContaSalario } from "./Conta/ContaSalario.js";
 
 const cliente1 = new Cliente("Ricardo", 11122233309);
 
