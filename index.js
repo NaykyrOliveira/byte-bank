65ec9892853ef556acc1884e3420d88bb3a27f6f
Naykyr Oliveira da Silva <103430603+NaykyrOliveira@users.noreply.github.com>
2022-11-03 10:46:56 -0300

    Polimofirsmo

--- index.js
+++ index.js
@@ -1,18 +1,13 @@
 import {Cliente} from "./Cliente.js";
-import {ContaCorrente} from "./Conta/ContaCorrente.js";
-import { ContaEmpresarial } from "./Conta/ContaEmpresarial.js";
-import {ContaPoupanca} from "./Conta/ContaPoupanca.js";
-import { ContaSalario } from "./Conta/ContaSalario.js";
+import { Gerente } from "./Funcionarios/Gerente.js";
+import { Diretor } from "./Funcionarios/Diretor.js";
+import { SistemaAutenticacao } from "./SistemaAutenticacao.js";
 
-const cliente1 = new Cliente("Ricardo", 11122233309);
+const diretor = new Diretor("Rodrigo", 10000, 12345678912);
+diretor.cadastrarSenha("123456789");
+const gerente = new Gerente("Ricardo", 5000, 12345678913);
+gerente.cadastrarSenha("987654321");
 
-const contaCorrenteRicardo = new ContaCorrente(cliente1, 1001);
-const contaPoupanca = new ContaPoupanca(50, cliente1, 1001);
-const contaSalario = new ContaSalario(cliente1);
-const contaEmpresarial = new ContaEmpresarial(cliente1);
+const estaLogado = SistemaAutenticacao.login(gerente, "98765432");
 
-contaEmpresarial.depositar(100);
-contaEmpresarial.sacar(10);
-
-console.log(contaEmpresarial);
-
+console.log(estaLogado);
