b5ea4271ab14bc3d3556ab536331c2eae4a9b949
Naykyr Oliveira da Silva <103430603+NaykyrOliveira@users.noreply.github.com>
2022-11-03 09:22:21 -0300

    Classses abstratas

--- Conta.js
+++ Conta.js
@@ -1,11 +1,13 @@
+// Classe abstrata (só funciona para ser herdada e nunca construída diretamente).
 export class Conta {
     constructor(saldoInicial, cliente, agencia) {
+        if(this.constructor == Conta) {
+            throw new Error("Você não deveria instanciar um objeto do tipo Conta Diretamente");
+        }
+
         this._saldo = saldoInicial;
         this._cliente = cliente;
         this._agencia = agencia;
-        if(this.constructor == Conta) {
-            console.log("Você não deveria instanciar um objeto do tipo conta");
-        }
     }
 
     set cliente(novoValor) {
--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const cliente1 = new Cliente("Ricardo", 11122233309);
 
 const ContaCorrenteRicardo = new ContaCorrente(cliente1, 1001);
 const contaPoupanca = new ContaPoupanca(50, cliente1, 1001);
-const conta = new Conta(0, cliente1, 1001);
+
 
 
 console.log(conta);
