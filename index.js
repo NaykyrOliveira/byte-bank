7e29a044ea24ba0fe629f7e37ed3bcedd34f19c6
Naykyr Oliveira da Silva <103430603+NaykyrOliveira@users.noreply.github.com>
2022-11-04 10:10:55 -0300

    Verificando propriedades e tipos

--- index.js
+++ index.js
@@ -3,11 +3,19 @@ import { Gerente } from "./Funcionarios/Gerente.js";
 import { Diretor } from "./Funcionarios/Diretor.js";
 import { SistemaAutenticacao } from "./SistemaAutenticacao.js";
 
+// Funcionaio
 const diretor = new Diretor("Rodrigo", 10000, 12345678912);
 diretor.cadastrarSenha("123456789");
 const gerente = new Gerente("Ricardo", 5000, 12345678913);
 gerente.cadastrarSenha("987654321");
+// Cliente
+const cliente = new Cliente("Manuel Gomes", 12345678920, "14789632");
 
-const estaLogado = SistemaAutenticacao.login(gerente, "98765432");
+const gerenteEstaLogado = SistemaAutenticacao.login(gerente, "987654321");
+const diretorEstaLogado = SistemaAutenticacao.login(diretor, "123456789");
 
-console.log(estaLogado);
+const clienteEstaLogado = SistemaAutenticacao.login(cliente, "14789632");
+
+console.log(gerenteEstaLogado, diretorEstaLogado, clienteEstaLogado);
+
+
